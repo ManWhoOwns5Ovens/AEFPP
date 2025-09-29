dcccc7df23ac5c322c57f7568f5b8d1a01fd2920
David Valsan <valsandavid2006@gmail.com>
2025-09-29 20:08:06 +0100

    Periodic back-up

--- DatabaseOps.py
+++ DatabaseOps.py
@@ -1,24 +1,44 @@
 import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 def runSqlCommand(command):
-    cursor.execute(command)
-    conn.commit()
+    pass
 
 def closeConnection():
-    conn.close()
+    pass
 
 def createTables():
-    runSqlCommand('CREATE TABLE FILES (' \
-    'ID int NOT NULL AUTO_INCREMENT' \
-    'PRIMARY KEY(ID)'\
-    'PATH string'
-    'NAME string'
-    'SIZE int'
-    'CREATED_AT DATETIME' 
-    ');')
+    runSqlCommand("""
+    CREATE TABLE FILES (
+        ID INT NOT NULL AUTO_INCREMENT,
+        PATH VARCHAR(255),
+        NAME VARCHAR(255),
+        SIZE INT,
+        CREATED_AT TIMESTAMP,
+        MD5_HASH VARCHAR(32),
+        SHA256_HASH VARCHAR(64),
+        STATUS VARCHAR(50),
+        PRIMARY KEY (ID)
+    );
+    """)
 
-conn=sqlite3.connect('aefpp.db')
-cursor = conn.cursor()
+    runSqlCommand("""
+    CREATE TABLE DOCUMENTS(
+        ID INT NOT NULL AUTO_INCREMENT,
+        FILE_ID INT NOT NULL AUTO_INCREMENT,
+        RAW_TEXT MEDIUMTEXT
+        NORMAL_TEXT MEDIUMTEXT
+        CHAR_COUNT INT
+        WORD_COUNT INT
+        UNIQUE_WORD_COUNT INT
+        KEYWORDS VARCHAR(255)
+        PRIMARY KEY(ID)
+        FOREIGN KEY(FILE_ID) REFERENCES FILES(ID)
+    );
+    """)
+
+engine= create_engine('sqlite:///aefpp.db')
 
 
 
